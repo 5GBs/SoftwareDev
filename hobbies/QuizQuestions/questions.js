b36039f63b2d2d82380d75178ff3900c0bce2c92
Adam Kerns <157764606+AdamKerns@users.noreply.github.com>
2025-03-01 18:40:38 -0500

    docs: questionnaire populates radio buttons and tracks user choices

--- hobbies/QuizQuestions/questions.js
+++ hobbies/QuizQuestions/questions.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function(){
     
 });
 
+// All of the questions for the quiz
 const questions = [
     "How do you prefer to spend your free time?",
     "Which of these activities sounds most appealing to you?",
@@ -16,6 +17,7 @@ const questions = [
     "How flexible are you with your hobby schedule?"
 ];
 
+// All of the answers for the quiz
 const answers = [
     [
         "Outdoors, exploring nature", 
@@ -89,27 +91,77 @@ const answers = [
     ]
 ];
 
-// for updating the question, answer, and question number on the html page
+// Updated as the user presses the next question button and holds the index of their choice
+const userChoices = ["", "", "", "", "", "", "", "", "", ""];
+
+// For updating the question, answer, and question number on the html page
 function updateQuestion() {
     const title = document.querySelector(".title");
     const but = document.querySelector(".nextQue");
     const answer = document.querySelector("#answers");
     const question = document.querySelector("#questions");
+
     question.style.padding = "20px";
     question.style.fontSize = "24px";
+
     let queNum = 1;
     title.innerHTML = "Question " + queNum + ":";
     question.innerHTML = questions[queNum - 1];
-    //TODO: figure out how to make the answers into radio buttons
-    answer.innerHTML = answers[queNum - 1];
+
+    //TODO: FORMAT THE RADIO BUTTONS
+
+    makeRadios(answers, queNum);
+    
     but.addEventListener("click", function() {
+        userChoices[queNum - 1] = checkRadios();
+        answer.innerHTML = "";
         queNum = queNum + 1;
+
         if (queNum > questions.length) {
             queNum = 10;
         }
+
         title.innerHTML = "Question " + queNum + ":";
         question.innerHTML = questions[queNum - 1];
-        answer.innerHTML = answers[queNum - 1];
+        makeRadios(answers, queNum);
+
+        console.log(userChoices);
     });
     
+}
+
+// Makes the radio buttons with the answer choices attached to them
+function makeRadios(answers, queNum) {
+    const answerLoc = document.querySelector("#answers");
+
+    for (let i = 0; i < answers[queNum - 1].length; i++) {
+        const radios = document.createElement("input");
+        const labels = document.createElement("label");
+
+        radios.setAttribute("type", "radio");
+        radios.setAttribute("id", "answer" + i);
+        radios.setAttribute("name", "question" + queNum); 
+        radios.value = i;
+
+        labels.setAttribute("for", "answer" + i);
+        labels.textContent = answers[queNum - 1][i];
+
+        answerLoc.appendChild(radios);
+        answerLoc.appendChild(labels);
+        answerLoc.appendChild(document.createElement("br"));
+    }
+}
+
+// See which radio button is checked and get the value associated with it for the results
+function checkRadios() {
+    const radios = document.querySelectorAll("input[type='radio']");
+    let selected = null;
+
+    for (const radio of radios) {
+        if (radio.checked) {
+            selected = radio.value;
+            break;
+        }
+    }
+    return selected;
 }
