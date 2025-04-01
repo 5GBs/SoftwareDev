2190e3a3f291095471305b576ca56ae47b755853
Michael Gohn <michaelgohn03@gmail.com>
2025-04-01 13:02:50 -0400

    added database_schema.sql

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)  # Create a Flask app instance
+app = Flask(__name__)
 
-@app.route('/')  # Define a route for the homepage
+@app.route('/')
 def home():
-    return "Hello, Flask!"  # Response when visiting '/'
+    return render_template('homepage.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)  # Run the Flask app
+    app.run(debug=True)
